34b0fa14602813f0e79db53e62a884f5a75ba2cc
ilikesounds <email_id>
2016-08-26 15:47:52 -0700

    modifying views for jinja

--- learning_journal_basic/views.py
+++ learning_journal_basic/views.py
@@ -1,32 +1,29 @@
-from pyramid.response import Response
+from pyramid.view import view_config
 import os
 
 
 HERE = os.path.dirname(__file__)
 
 
+@view_config(route_name='list_view', renderer='')
 def list_view(request):
     imported_text = open(os.path.join(HERE, './templates/index.html')).read()
     return Response(imported_text)
 
 
+@view_config(route_name='detail_view', renderer='')
 def detail_view(request):
     imported_text = open(os.path.join(HERE, './templates/detail.html')).read()
     return Response(imported_text)
 
 
+@view_config(route_name='entry_view', renderer='')
 def entry_view(request):
     imported_text = open(os.path.join(HERE, './templates/entry.html')).read()
     return Response(imported_text)
 
 
+@view_config(route_name='edit_view', renderer='')
 def edit_view(request):
     imported_text = open(os.path.join(HERE, './templates/edit.html')).read()
     return Response(imported_text)
-
-
-def includeme(config):
-    config.add_view(list_view, route_name='list_view')
-    config.add_view(detail_view, route_name='detail_view')
-    config.add_view(entry_view, route_name='entry_view')
-    config.add_view(edit_view, route_name='edit_view')
