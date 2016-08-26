916c2fb2e78fb1e76a0c2ab682a08aab97c495fd
ilikesounds <email_id>
2016-08-25 18:23:04 -0700

    ready to deploy to heroku

--- learning_journal_basic/routes.py
+++ learning_journal_basic/routes.py
@@ -5,4 +5,7 @@ def includeme(config):
     """This function adds routes to Pyramid's Configurator
     """
     config.add_static_view('static', 'static', cache_max_age=3600)
-    config.add_route('home', '/')
+    config.add_route('list_view' '/')
+    config.add_route('detail_view' '/journal/{id:\d+}')
+    config.add_route('entry_view' '/journal/new_entry')
+    config.add_route('edit_view' '/journal/{id:\d+}')
