c7045ce00a1b82bb4da1053de41601e4a95226dc
ilikesounds <email_id>
2016-08-28 18:47:03 -0700

    finished tests

--- learning_journal_basic/tests.py
+++ learning_journal_basic/tests.py
@@ -3,13 +3,30 @@ import pytest
 from pyramid import testing
 
 
+def test_entry_view():
+    from .views import entry_view
+    request = testing.DummyRequest()
+    info = entry_view(request)
+    assert 'entries' in info
+
+
 def test_detail_view():
     from .views import detail_view
     request = testing.DummyRequest()
+    request.matchdict = {'id': '1'}
     info = detail_view(request)
-    assert "title" in info
+    assert 'body' in info
+
+
+def test_edit_view():
+    from .views import edit_view
+    request = testing.DummyRequest()
+    request.matchdict = {'id': '1'}
+    info = edit_view(request)
+    assert 'body' in info
 
 # ------- Functional Tests -------
+
 
 @pytest.fixture()
 def testapp():
@@ -18,9 +35,11 @@ def testapp():
     from webtest import TestApp
     return TestApp(app)
 
+
 def test_layout_root(testapp):
     response = testapp.get('/', status=200)
-    assert b'This is text for test entry' in response.body
+    assert b'jeff torres' in response.body
+
 
 def test_root_contents(testapp):
     response = testapp.get('/', status=200)
