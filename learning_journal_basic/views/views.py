76c984434d203908cc0e64da7a1ca4ee5cf5eb51
ilikesounds <email_id>
2016-09-12 00:46:34 -0700

    routes and views completed

--- learning_journal_basic/views/views.py
+++ learning_journal_basic/views/views.py
@@ -5,7 +5,8 @@ Views controller for Pyramid Learning Journal application
 from pyramid.view import view_config
 from pyramid.httpexceptions import HTTPNotFound
 from pyramid.httpexceptions import HTTPFound
-import datetime
+import pytz
+from datetime import datetime
 
 from ..models import PLJ_Article
 
@@ -14,9 +15,9 @@ def list_view(request):
     """
     This will return the index page for the Learning Journal
     """
-    query = request.dbsession.query(PLJ_Article).order_by(PLJ_Article.date_created.desc())
-    entries = query.all()
-    return {'entries': entries}
+    articles = request.dbsession.query(PLJ_Article).order_by(PLJ_Article.date_created.desc())
+    article = articles.all()
+    return {'articles': article}
 
 
 @view_config(route_name='detail_view', renderer='templates/detail.jinja2')
@@ -24,9 +25,11 @@ def detail_view(request):
     """
     This will return the detail page for an article in the Learning Journal
     """
-    for entry in ENTRIES:
-        if entry['id'] == int(request.matchdict['id']):
-            return entry
+    try:
+        article = request.dbsession.query(PLJ_Article).get(request.matchdict['id'])
+    except DBAPIError:
+        return Response(db_err_msg, content_type='text/plain', status=500)
+    return {'article': article}
 
 
 @view_config(route_name='entry_view', renderer='../templates/entry.jinja2')
@@ -35,6 +38,16 @@ def entry_view(request):
     This will return the article entry page for new article in the Learning
     Journal
     """
+    if request.method == "POST":
+        new_title = request.POST["title"]
+        new_body = request.POST["body"]
+        time = datetime.now(pytz.utc)
+        new_model = PLJ_Article(title=new_title, body=new_body, date_created=time)
+
+        request.dbsession.add(new_model)
+
+        return HTTPFound(request.route_url("home"))
+
     return {}
 
 
@@ -44,6 +57,17 @@ def edit_view(request):
     This will return the article edit page for an article in the Learning
     Journal
     """
-    for entry in ENTRIES:
-        if entry['id'] == int(request.matchdict['id']):
-            return entry
+    if request.method == "POST":
+        new_title = request.POST["title"]
+        new_body = request.POST["body"]
+        time = datetime.now(pytz.utc)
+        new_model = PLJ_Article(title=new_title, body=new_body, date_created=time)
+
+        request.dbsession.add(new_model)
+
+        return HTTPFound(request.route_url("home"))
+    try:
+        article = request.dbsession.query(PLJ_Article).get(request.matchdict['id'])
+    except DBAPIError:
+        return Response(db_err_msg, content_type='text/plain', status=500)
+    return {'article': article}
