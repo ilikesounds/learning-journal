e598ce3c2f97a9f6d1192f36c05ae3feafcd4c07
ilikesounds <email_id>
2016-09-13 20:46:38 -0700

    cannot get tests to work

---
+++ learning_journal_basic/tests/tests.py
@@ -0,0 +1,47 @@
+import pytest
+
+from pyramid import testing
+
+
+def test_entry_view():
+    from ..views.default import entry_view
+    request = testing.DummyRequest()
+    info = entry_view(request)
+    assert 'entries' in info
+
+
+def test_detail_view():
+    from ..views.default import detail_view
+    request = testing.DummyRequest()
+    request.matchdict = {'id': '1'}
+    info = detail_view(request)
+    assert 'body' in info
+
+
+def test_edit_view():
+    from ..views.default import edit_view
+    request = testing.DummyRequest()
+    request.matchdict = {'id': '1'}
+    info = edit_view(request)
+    assert 'body' in info
+
+# ------- Functional Tests -------
+
+
+@pytest.fixture()
+def testapp():
+    from learning_journal_basic import main
+    settings = {'sqlalchemy.url': 'sqlite db path'}
+    app = main({}, **settings)
+    from webtest import TestApp
+    return TestApp(app)
+
+
+def test_layout_root(testapp):
+    response = testapp.get('/', status=200)
+    assert b'jeff torres' in response.body
+
+
+def test_root_contents(testapp):
+    response = testapp.get('/', status=200)
+    assert b'<article>' in response.body
