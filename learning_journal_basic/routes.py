b370e0da3859e88a918a761bede5c094e7856c24
ilikesounds <jeffrey.n.torres@gmail.com>
2016-10-31 18:58:33 -0700

    added new routes for login and logout and list_view

--- learning_journal_basic/routes.py
+++ learning_journal_basic/routes.py
@@ -6,6 +6,9 @@ def includeme(config):
     """
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
+    config.add_route('login', '/login')
+    config.add_route('logout', '/logout')
+    config.add_route('list_view', '/journal/list')
     config.add_route('detail_view', '/journal/{id:\d+}')
     config.add_route('entry_view', '/journal/new_entry')
     config.add_route('edit_view', '/journal/edit_view/{id:\d+}')
